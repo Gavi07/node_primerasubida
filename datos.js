8811612360076a0854821ffd81d7884307860cbc
gavi07 <unicyuri@gmail.com>
2019-04-14 18:09:43 -0500

    Se agregan primeros archivos del proyecto

---
+++ datos.js
@@ -0,0 +1,31 @@
+const opciones = {
+    nombre: {
+        alias: 'n',
+        demand: true
+    },
+    matematicas: {
+        alias: 'm',
+        demand: true
+    },
+    ingles: {
+        alias: 'i',
+        demand: true
+    },
+    programacion: {
+        alias: 'p',
+        demand: true
+    }
+}
+
+let obtenerPromedio = (nota_uno, nota_dos, nota_tres) => {
+    let resultado = (nota_uno + nota_dos + nota_tres)/3;
+};
+
+const argv = require('yargs')
+                .command('promedio', 'Calcular el promedio')
+                .argv;
+
+module.exports = {
+    obtenerPromedio,
+    argv
+};
